5c681f80e574930dfa3a56a81f326467f526ceaf
vanisendeveloper <vanisendeveloper@gmail.com>
2019-10-21 12:12:34 -0400

    Heroku supports only PORT

--- src/server.js
+++ src/server.js
@@ -4,8 +4,9 @@ var static = require('node-static');
 // Create a node-static server instance to serve the './public' folder
 //
 var file = new static.Server('dist');
+const PORT = process.env.PORT || 3000;
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
-}).listen(8000);
+}).listen(PORT);
